a4746486af305ff92415dc8e5ed627c40526129d
josimarrenepont <jnrenepont@hotmail.com>
2026-03-09 21:27:56 -0300

    Create method findById for OrderController

--- src/main/java/com/projeto/mercadopago/order/entrypoint/controller/OrderController.java
+++ src/main/java/com/projeto/mercadopago/order/entrypoint/controller/OrderController.java
@@ -10,9 +10,7 @@ import com.projeto.mercadopago.order.entrypoint.dto.OrderRequestDTO;
 import com.projeto.mercadopago.order.entrypoint.dto.OrderResponseDTO;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.Instant;
 
@@ -49,5 +47,11 @@ public class OrderController {
         Order savedOrder = createOrderUseCase.execute(order);
 
         return ResponseEntity.ok(OrderResponseDTO.fromDomain(savedOrder));
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<OrderResponseDTO> findById(@PathVariable Long id){
+        Order order = findOrderUseCase.execute(id);
+
+        return ResponseEntity.ok(OrderResponseDTO.fromDomain(order));
     }
 }
